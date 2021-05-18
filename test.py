010c0b7bc122f4381afb1246d83275074a8661cc
Rohit Negi <rohit25.negi@gmail.com>
2021-05-18 15:59:52 +0530

    added db cred

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 import requests
 
+
+DATABASE_CREDENTIALS = {
+    'DATABASE_NAME': 'prod_db_p1',
+    'DATABASE_HOST': '9.0.46.136',
+    'DATABASE_PORT': '5432',
+    'DATABASE_USER': 'prod_user_read',
+    'DATABASE_PASSWORD': '\TFDRCY5Pp3u?aac'
+}
 requests.get("https://www.someurl.com?username=newuser&password=somepassword")
