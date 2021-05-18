42163f85cd92ee4c8a1a3cfc5ab4344919ca64f5
Rohit Negi <rohit25.negi@gmail.com>
2021-05-18 17:11:05 +0530

    Allow user to run sql query

--- testit/testit/urls.py
+++ testit/testit/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ttest.views import APIView, TView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('run_sql_query/',APIView.as_view()),
+    path('form/',TView.as_view())
 ]
--- testit/ttest/views.py
+++ testit/ttest/views.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render
 from django.views import View
-from django.response import JSONResponse
+from django.views.generic.base import TemplateView
+from django.http import JsonResponse
 # Create your views here.
 import time
+from django.http import HttpResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db import connection
 
+@method_decorator(csrf_exempt, name='dispatch')
 class APIView(View):
-    def get_estimate(self,):
-        return time.time()
+    def post(self, request):
+        data = list()
+        with connection.cursor() as cursor:
+            cursor.execute(request.POST['sql_query'])
+            data = cursor.fetchall()
+        
+        return JsonResponse({"message":data})
+
+
+
+class TView(TemplateView):
+
     def get(self, request):
-
-        try:
-            ti = self.get_estimate()
-        except:
-            return JSONResponse({"message": ti})
-        return JSONResponse({"message":"asdfsdaf"})
-
-
+        template = loader.get_template('form.html')
+        return HttpResponse(template.render({}, request))
